3e79776417c2216420eca6a3b2e13d3b1b5ef135
Tanisha2910 <37878240+Tanisha2910@users.noreply.github.com>
2018-09-05 21:16:49 +0530

    Update AnnagramStrings.java

--- AnnagramStrings.java
+++ AnnagramStrings.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Check {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
  Scanner sc = new Scanner(System.in);
  String a = sc.next();
  String b = sc.next();
