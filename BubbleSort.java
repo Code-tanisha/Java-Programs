0e7e47f66ed5e28b7d76159b3950cf1ef3b05c46
Tanisha2910 <37878240+Tanisha2910@users.noreply.github.com>
2018-09-05 21:17:28 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -24,7 +24,7 @@ public class Sort {
 		}
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
  Scanner sc = new Scanner(System.in);
  int n = sc.nextInt();
  int arr[] = new int[n];
