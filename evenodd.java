902de90ed514aac0bb3b9745849c617769720f2b
Tanisha2910 <37878240+Tanisha2910@users.noreply.github.com>
2018-09-05 21:19:00 +0530

    Update evenodd.java

--- evenodd.java
+++ evenodd.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
  Scanner sc = new Scanner(System.in);
  int n = sc.nextInt();
  if(n%2==0) {
