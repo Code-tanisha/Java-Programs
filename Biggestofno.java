ca1b317302e7eb67205e7f75e929599b2fe7fdba
Tanisha2910 <37878240+Tanisha2910@users.noreply.github.com>
2018-09-05 21:18:01 +0530

    Update Biggestofno.java

--- Biggestofno.java
+++ Biggestofno.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 Scanner sc = new Scanner(System.in);
 // Take two no. as input
 int a = sc.nextInt();
