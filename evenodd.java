f34ccae656e041fd7aea0abef4f9b1157ee083dc
Tanisha2910 <37878240+Tanisha2910@users.noreply.github.com>
2018-09-05 19:29:03 +0530

    program for even odd

---
+++ evenodd.java
@@ -0,0 +1,18 @@
+// This program is for even odd.
+// input: any number like 10
+// output: Even/Odd. ex: Even
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Scanner sc = new Scanner(System.in);
+ int n = sc.nextInt();
+ if(n%2==0) {
+	 System.out.println("Even");
+ }
+ System.out.println("Odd");
+	}
+
+}
