5178063c4b1c7a2e46b008789aabf1c49313a7ea
Tanisha Gupta <37878240+Tanisha2910@users.noreply.github.com>
2018-10-10 19:08:21 +0530

    Program is to print common elements among 2 Array.

---
+++ Commonelements.java
@@ -0,0 +1,20 @@
+//Program is to print common elements in 2 arrays.
+//Input: 2 arrays.
+//Output: Print common elements.
+
+import java.util.HashSet;
+public class CommonElements {
+ public static void main(String[] args) {
+		String[] s1 = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "FOUR"};
+		String[] s2 = {"THREE", "FOUR", "FIVE", "SIX", "SEVEN"};
+		HashSet <String> set = new HashSet<String>();
+		for(int i = 0; i < s1.length; i++) {
+			for(int j = 0; j < s2.length; j++) {
+				if(s1[i].equals(s2[j])) {
+				set.add(s1[i]);
+				}
+			}
+		}
+		System.out.println(set);
+    }
+}
